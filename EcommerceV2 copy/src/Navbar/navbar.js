99ab850f9ef71bedf8fde4998809e1bbfc71c1ff
Skyler Higgins <Kojiro>
2020-06-22 16:51:01 -0400

    Added Empty React

--- EcommerceV2 copy/src/Navbar/navbar.js
+++ EcommerceV2 copy/src/Navbar/navbar.js
@@ -1,20 +1,22 @@
 import React from "react"
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 const Nav = () => {
 
-    return (
-      <header className="header">
-        <img className="logo" src="Cars/logo.jpeg" alt="logo" /><a href=""></a><a href=""></a>
-          <ul className="nav">
-            <li><Link className="current" to="/">Home</Link></li>
-            <li><Link to="/inventory">Inventory</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </header>       
-    );
-  }
+  return (
+    <header className="navbar">
+      <img className="logo" src="Cars/logo.jpeg" alt="logo" />
+      <ul className="nav">
+        <li><Link className="current" to="/">Home</Link></li>
+        <li><Link to="/Gaming">Gaming</Link></li>
+        <li><Link to="/Science">Science</Link></li>
+        <li><Link to="/Space">Space</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </header>
+  );
+}
 
 
 export default Nav;
