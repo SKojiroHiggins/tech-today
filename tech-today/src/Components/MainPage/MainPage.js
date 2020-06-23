b6c11b54230308a922aa13bc57e52e15694efc03
Skyler Higgins <Kojiro>
2020-06-23 09:39:50 -0400

    Nav bar and routes to all pages

---
+++ tech-today/src/Components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const MainPage = () => {
+    return (
+        <div>
+            <Navbar page="mainPage"/>
+        </div>
+    )
+};
+
+export default MainPage;
---
+++ tech-today/src/Components/SpacePage/SpacePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const SpacePage = () => {
+    return (
+        <div>
+            <Navbar page="spacePage"/>
+        </div>
+    )
+};
+
+export default SpacePage;
