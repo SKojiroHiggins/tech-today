e629bbc7730356ea60cee2015fd31524f403ed26
J.guzman <61435395+jguzman11@users.noreply.github.com>
2020-06-24 12:13:26 -0400

    content

--- tech-today/src/Components/ContentHero/contentHero.js
+++ tech-today/src/Components/ContentHero/contentHero.js
@@ -1,18 +1,17 @@
 import React from 'react';
-// import '../App.css';
 import Card from 'react-bootstrap/Card';
+import contentHero from '../ContentHero/ContentHero.css'
 
 
-
-function contentHero() {
+function ContentHero() {
   return (
     <div className="hero">
       <div className='content'>
         <Card>
-          <Card.Header>Tech</Card.Header>
+          <Card.Header>Gaming</Card.Header>
           <Card.Body>
-            <Card.Text className="blockquote mb-0">
-              <p>
+            <Card.Text>
+              <p className='content-text'>
                 The latest tech news about the world's best (and sometimes worst) hardware, apps, and much more. From top companies like Google and Apple to tiny startups vying for your attention, Verge Tech has the latest in what matters in technology daily.
               </p>
             </Card.Text>
@@ -22,11 +21,10 @@ function contentHero() {
       <span></span>
       <div className='vid-box'>
         <iframe className='video-its'
-          src="https://www.youtube.com/embed/tgbNymZ7vqY">
-        </iframe>
+          src="https://www.youtube.com/embed/_Q8AKghK44M" />
       </div>
     </div>
   );
 }
 
-export default contentHero;
+export default ContentHero;
