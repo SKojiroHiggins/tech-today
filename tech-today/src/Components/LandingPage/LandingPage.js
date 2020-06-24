564dbb2e776c99811ae6cffeff84bcf05643d28d
Skyler Higgins <Kojiro>
2020-06-24 15:45:18 -0400

    Landing Page

---
+++ tech-today/src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LandingPage = () => {
+    return (
+        <div>
+            <div className="wrapper">
+                <div className="masthead">
+                    <div className="content">
+                        <h1>Tech Today</h1>
+                        <h3>Gaming/Science/Space</h3>
+                        <Link to="/home" className="button">START!</Link>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+};
+
+export default LandingPage;
--- tech-today/src/Components/Routes/Routes.js
+++ tech-today/src/Components/Routes/Routes.js
@@ -1,23 +1,24 @@
 import React from 'react';
 // import ProductPage from '../ProductsPage/ProductsPage';
 // import Home from '../HomePage/HomePage'
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import ContactPage from '../ContactPage/ContactPage';
 import MainPage from '../MainPage/MainPage';
 import GamingPage from '../GamingPage/GamingPage';
 import SciencePage from '../SciencePage/SciencePage';
 import SpacePage from '../SpacePage/SpacePage';
-
+import LandingPage from '../LandingPage/LandingPage';
 
 const routes = () => {
-    return(
+    return (
         <Router>
             <div>
-                <Route exact path='/' component={MainPage}/>
-                <Route exact path='/gaming' component={GamingPage}/>
-                <Route exact path='/contact' component={ContactPage}/>
-                <Route exact path='/science' component={SciencePage}/>
-                <Route exact path='/space' component={SpacePage}/>
+                <Route exact path='/' component={LandingPage} />
+                <Route exact path='/home' component={MainPage} />
+                <Route exact path='/gaming' component={GamingPage} />
+                <Route exact path='/contact' component={ContactPage} />
+                <Route exact path='/science' component={SciencePage} />
+                <Route exact path='/space' component={SpacePage} />
 
             </div>
         </Router>
