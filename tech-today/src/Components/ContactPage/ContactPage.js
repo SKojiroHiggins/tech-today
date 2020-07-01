7d39ab4c68fed98854361255b9b940c4083d75b8
Darwin Bueso <darwingaldamez5@gmail.com>
2020-07-01 00:38:40 -0400

    Updating

--- tech-today/src/Components/ContactPage/ContactPage.js
+++ tech-today/src/Components/ContactPage/ContactPage.js
@@ -22,7 +22,7 @@ const contactPage = () => {
                     </div>
                 </div>
                 <div className="darwinFormContainer">
-                    <form className="darwinForm" name="contact" method="POST" action="https://formsubmit.co/darwingaldamez5@gmail.com">
+                    <form className="darwinForm" name="contact" method="POST">
                         <label for="fname">First Name</label>
                         <input type="text" className="fname" id="input-0" name="firstname" placeholder="Your name.." required />
 
