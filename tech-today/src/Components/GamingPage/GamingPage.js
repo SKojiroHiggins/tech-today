a493331006e0fd586e84642213c99947cc9853a7
Skyler Higgins <Kojiro>
2020-07-08 14:50:14 -0400

    bug fixes

--- tech-today/src/Components/GamingPage/GamingPage.js
+++ tech-today/src/Components/GamingPage/GamingPage.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
-<<<<<<< HEAD
 import ContentHero from '../ContentHeros/GameHero/GameHero';
-=======
-import ContentHero from '../ContentHero/contentHero';
-import Carousel from '../Carousel/GamingCarousel'
->>>>>>> 446285147fa491e97bc21b1d4e0e4256eed9bdc9
 import Footer from '../Footer/Footer';
 import GameContent from '../Content/Gamers/GameContent';
 import Gaming from '../Content/Gamers/gaming';
